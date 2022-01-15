0a9dd00f03e044ad23b29e7597da5b6058947735
jiangsir <jiangzero@gmail.com>
2022-01-15 15:43:21 +0800

    commit

--- 範例6. 密碼檢查.py
+++ 範例6. 密碼檢查.py
@@ -4,7 +4,7 @@ password2 = input('請輸入密碼: ')
 while password != password2 and times < 3:
     print('密碼錯誤!')
     times = times + 1
-    password2 = input('請再次輸入密碼: ')
+    password2 = input('請輸入密碼: ')
 
 if password == password2:
     print('歡迎使用本系統')
--- 範例7. 任意數所有因數.py
+++ 範例7. 任意數所有因數.py
@@ -1,7 +1,7 @@
 n = int(input('請輸入數字 n: '))
 factors = []
 for i in range(1, n+1):
-    if n%i==0:
+    if n % i == 0:
         factors.append(i)
 print(factors)
-print(factors[0])
+print(', '.join(map(str, factors)))
